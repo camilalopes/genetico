12e68fe10c7fd1336d64e7a4688d6bcc2acbf890
Camila Lopes <kmilalopes10@gmail.com>
2019-05-07 11:45:11 -0300

    Adiciona método de votação no Knn

--- aprendizagem/knn.py
+++ aprendizagem/knn.py
@@ -1,5 +1,7 @@
 class Knn():
     k = 1
+    # Representação do tipo elemento, classe
+    elementos: list = []
 
     def __init__(self, k):
         self.k = k
@@ -9,6 +11,9 @@ class Knn():
         for i in range(len(vetor_1)):
             distancia += (vetor_1[i] - vetor_2[i]) ** 2
         return distancia ** (1/2)
+    
+    def retorna_distancia(self, elemento) -> float:
+        return elemento[0]
 
     def get_vizinhos(self, centro_consulta, elementos) -> list:
         vizinhos: list = []
@@ -23,5 +28,16 @@ class Knn():
 
         return k_vizinhos
 
-    def retorna_distancia(self, elemento) -> float:
-        return elemento[0]
+    def votacao(self, vizinhos) -> str:
+        classes = {}
+        for v in vizinhos:
+            if v[1] in classes.keys():
+                classes[v[1]] += 1
+            else:
+                classes[v[1]] = 1
+        mais_votado = max(classes.keys(), key=(lambda i: classes[i]))
+        return mais_votado
+    
+    def treina(self, elementos) -> None:
+        self.elementos = elementos
+    
